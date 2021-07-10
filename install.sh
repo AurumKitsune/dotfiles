2bb6124797f2383c134a5b9f8d297f2bee880465
Aurum <AurumKitsune@gmail.com>
2021-07-10 15:42:44 -0500

    Fixed issues in script and added pacman stuff

--- install.sh
+++ install.sh
@@ -6,5 +6,14 @@ then
     exit
 fi
 
-cd ~/
-~/grub/install.sh
+cd grub
+./install.sh
+cd
+
+read -p "Would you like to run pacman commands? (y/n): "
+if [ $REPLY == 'y' ]
+then
+    echo "running pacman"
+    pacman -Syy
+    pacman -S kitty
+fi
